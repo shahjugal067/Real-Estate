c91901f41660beeeb97594840213c355da7e12b4
shahjugal067 <shahjugal067@gmail.com>
2025-03-01 19:02:15 +0545

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,30 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js'
+import cors from 'cors';
+
+dotenv.config();
+
 
 
 const app = express();
 
+connectDB();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Server Error ';
+    return res.status(statusCode).json({success: false,statusCode, message });
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');

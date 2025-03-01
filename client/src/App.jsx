c580dfca75f5b324ab9b77275fa503c591504892
shahjugal067 <shahjugal067@gmail.com>
2025-03-01 11:52:03 +0545

    changes

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/Signin'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
+
+const App = () => {
+  return (
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+
+    </BrowserRouter>
+  )
+}
+
+export default App
